7d7ef13b3019db577689886cdef5cbe589015cc6
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-10-03 14:50:17 -0500

    added client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LinkList from './screens/LinkList/LinkList.tsx';
+
+function App() {
+
+  return (
+    <>
+      <LinkList />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/screens/LinkList/LinkList.tsx
@@ -0,0 +1,10 @@
+const LinkList = () => {
+    
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    );
+};
+
+export default LinkList;
