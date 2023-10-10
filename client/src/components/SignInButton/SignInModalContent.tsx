0e307120bf9507d64e9436adbb65998a1d0c0c83
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-10-10 13:35:47 -0500

    created custom inputs

--- client/src/components/SignInButton/SignInModalContent.tsx
+++ client/src/components/SignInButton/SignInModalContent.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import SignUpModalContent from '../SignUpButton/SignUpModalContent.tsx';
 import useSignInForm from './useSignInForm.ts';
 import SubmitButton from '../SubmitButton.tsx';
+import Input from '../Input.tsx';
 
 const SignInModalContent = () => {
     const { onSubmit, register, errors } = useSignInForm();
@@ -13,25 +14,12 @@ const SignInModalContent = () => {
                 <>
                     <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
                     <form onSubmit={onSubmit} className="space-y-6" action="#">
-                        {/*<div className="relative z-0">
-                            <input id="floating_standard" {...register('email')} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                            <span className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your email</span>
-                            { errors.email && (<span className="text-red-500 text-sm">{errors.email.message}</span>) }
-                        </div>*/}
-                        <div>
-                            <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</span>
-                            <input type="email" {...register('email')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" />
-                            { errors.email && (<span className="text-red-500 text-sm">{errors.email.message}</span>) }
-                        </div>
-                        <div>
-                            <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</span>
-                            <input type="password" {...register('password')} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
-                            { errors.password && (<span className="text-red-500 text-sm">{errors.password.message}</span>) }
-                        </div>
+                        <Input type="email" errorMessage={errors.email?.message} label='Your email' placeholder="name@company.com" rest={{...register('email')}} />
+                        <Input type="password" errorMessage={errors.password?.message} label='Your password' placeholder="name@company.com" rest={{...register('password')}} />
                         <div className="flex justify-between">
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
-                                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+                                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-cyan-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-cyan-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
                                 </div>
                                 <span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</span>
                             </div>
--- client/src/components/SignUpButton/SignUpModalContent.tsx
+++ client/src/components/SignUpButton/SignUpModalContent.tsx
@@ -1,5 +1,6 @@
 import useSignUpForm from './useSignUpForm.ts';
 import SubmitButton from '../SubmitButton.tsx';
+import Input from '../Input.tsx';
 
 const SignUpModalContent = () => {
    const { onSubmit, register, errors } = useSignUpForm();
@@ -8,26 +9,10 @@ const SignUpModalContent = () => {
         <>
             <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign up with your email address</h3>
             <form onSubmit={onSubmit} className="space-y-6" action="#">
-                <div>
-                    <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your nickname</span>
-                    <input type="email" {...register('nickname')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" />
-                    { errors.nickname && (<span className="text-red-500 text-sm">{errors.nickname.message}</span>) }
-                </div>
-                <div>
-                    <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</span>
-                    <input type="email" {...register('email')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" />
-                    { errors.email && (<span className="text-red-500 text-sm">{errors.email.message}</span>) }
-                </div>
-                <div>
-                    <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</span>
-                    <input type="password" {...register('password')} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
-                    { errors.password && (<span className="text-red-500 text-sm">{errors.password.message}</span>) }
-                </div>
-                <div>
-                    <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</span>
-                    <input type="password" {...register('confirmPassword')} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
-                    { errors.confirmPassword && (<span className="text-red-500 text-sm">{errors.confirmPassword.message}</span>) }
-                </div>
+                <Input type="text" errorMessage={errors.nickname?.message} label='Your nickname' placeholder="Steven Reznik" rest={{...register('nickname')}} />
+                <Input type="email" errorMessage={errors.email?.message} label='Your email' placeholder="name@company.com" rest={{...register('email')}} />
+                <Input type="password" errorMessage={errors.password?.message} label='Your password' placeholder="••••••••" rest={{...register('password')}} />
+                <Input type="password" errorMessage={errors.confirmPassword?.message} label='Confirm password' placeholder="••••••••" rest={{...register('confirmPassword')}} />
                 <SubmitButton className="w-full" type="submit">Create account</SubmitButton>
             </form>
         </>
