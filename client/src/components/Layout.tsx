8e74415a1765fff298ed5f97ab2bc8b57214fc31
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-10-29 14:38:55 -0500

    fixed width for layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from 'react';
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <div className="flex items-center justify-center">
-            <div className="flex flex-col items-center md:px-24 max-w-screen-2xl md:w-[95%] pb-24">
+            <div className="flex flex-col items-center sm:px-4 md:px-24 max-w-screen-2xl pb-24 min-w-[300px]">
                 {children}
             </div>
         </div>
