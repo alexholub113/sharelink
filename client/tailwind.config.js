c825c555326a2efb1d0dc81561ef6bf616666429
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-11-18 10:24:54 -0600

    removed flowbite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,15 +3,9 @@ export default {
   darkMode: 'class',
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
 }
-
