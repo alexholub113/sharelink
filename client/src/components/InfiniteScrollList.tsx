326eb94af1c61a5ed4307834c624cab5c81df479
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-11-27 11:01:09 -0600

    added InfiniteScrollList component

---
+++ client/src/components/InfiniteScrollList.tsx
@@ -0,0 +1,63 @@
+﻿import * as React from 'react';
+import { useRef, useCallback } from 'react';
+
+type InfiniteScrollListProps = {
+    isLoading: boolean;
+    isReloading: boolean;
+    renderLoader: (isLoaderActive: boolean) => React.ReactNode | null;
+    threshold?: number;
+    hasNextPage: boolean;
+    fetchNextPage: () => void;
+};
+
+const InfiniteScrollList: React.FC<React.PropsWithChildren<InfiniteScrollListProps>> = (props) => {
+    const {
+        children,
+        isLoading,
+        isReloading,
+        renderLoader,
+        hasNextPage,
+        fetchNextPage,
+        threshold = 3,
+    } = props;
+
+    const observer = useRef<IntersectionObserver>();
+
+    const lastRowElementRef = useCallback((node: Element) => {
+        if (isLoading || isReloading || !hasNextPage) return;
+        if (observer.current) observer.current.disconnect();
+
+        if (!observer.current) {
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting) {
+                    fetchNextPage();
+                }
+            });
+        }
+
+        if (node) {
+            observer.current.observe(node);
+        }
+    }, [isLoading, isReloading, props.hasNextPage]);
+
+    const childrenCount = React.Children.count(children);
+
+    return (
+        <>
+            { !isReloading && React.Children.map(children, (child, index) => {
+                if (!React.isValidElement(child) || childrenCount - threshold !== index + 1) {
+                    return child;
+                }
+
+                return React.cloneElement(child, {
+                    ...child.props,
+                    ref: lastRowElementRef,
+                });
+            }) }
+
+            { renderLoader(isLoading || isReloading) }
+        </>
+    );
+};
+
+export default InfiniteScrollList;
