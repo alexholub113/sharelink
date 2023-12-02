b94f4bab13fedb414977bb9dcf6a5dc983cfb2f0
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-12-02 08:10:42 -0600

    added ScrollUpTrigger

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import AddLinkScreen from './screens/AddLink/AddLinkScreen.tsx';
 import {useUserStore} from './contexts/AppContext.tsx';
 import {observer} from 'mobx-react-lite';
 import Footer from './components/Footer.tsx';
+import ScrollUpTrigger from './components/ScrollUpTrigger.tsx';
 
 const App = observer(() => {
     const [section, setSection] = useState<'list' | 'addLink'>('list');
@@ -17,21 +18,24 @@ const App = observer(() => {
     }, [init]);
 
     return (
-        <div className="bg-zinc-900 flex flex-col justify-start min-h-screen">
-            <div className="flex-grow">
-                <Toolbar />
-                <Layout>
-                    <>
-                        <div className="mb-8">
-                            <AddLinkButton showAddLink={section == 'list'} onClick={() => setSection(section === 'list' ? 'addLink' : 'list')} />
-                        </div>
-                        { section === 'list' && <LinkListScreen /> }
-                        { section === 'addLink' && <AddLinkScreen onSuccess={() => setSection('list')} /> }
-                    </>
-                </Layout>
+        <>
+            <div className="bg-zinc-900 flex flex-col justify-start min-h-screen">
+                <div className="flex-grow">
+                    <Toolbar />
+                    <Layout>
+                        <>
+                            <div className="mb-8">
+                                <AddLinkButton showAddLink={section == 'list'} onClick={() => setSection(section === 'list' ? 'addLink' : 'list')} />
+                            </div>
+                            { section === 'list' && <LinkListScreen /> }
+                            { section === 'addLink' && <AddLinkScreen onSuccess={() => setSection('list')} /> }
+                        </>
+                    </Layout>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+            <ScrollUpTrigger />
+        </>
     )
 });
 
